47ee75c402f076791e604e6c685601622f06e39a
Przemysław Uznański <przemek@pathway.com>
2024-03-07 15:41:18 +0100

    [LaPoste] fixes to pipeline (better complexity) (#5867)
    * .
    
    * better typing
    
    * .
    
    * .
    
    * .
    
    * cleanup
    
    * pytest
    
    * .
    
    * .
    
    * .
    
    * .
    
    * fixes and refactors
    
    * final touches
    
    * alerting to connector
    
    * with a flag
    
    * cleanup
    
    * handlers
    
    * Update public/pathway/python/pathway/xpacks/spatial/geofencing.py
    
    Co-authored-by: Mateusz Lewandowski <lewymati@users.noreply.github.com>
    
    * any
    
    * Update projects/pathway/realtime/alerting.py
    
    Co-authored-by: Mateusz Lewandowski <lewymati@users.noreply.github.com>
    
    * Update projects/pathway/realtime/geofencing.py
    
    Co-authored-by: Mateusz Lewandowski <lewymati@users.noreply.github.com>
    
    * Update k8s/pathway/deploys/laposte-k3s/laposte-full-rt.yaml
    
    * Update projects/pathway/realtime/alerting.py
    
    * lat lon
    
    * Update projects/pathway/realtime/geofencing.py
    
    * .
    
    * parametrizing
    
    * .
    
    * black
    
    * smarter processing
    
    * quickfix
    
    * fix to the test
    
    * missing file
    
    * .
    
    ---------
    
    Co-authored-by: Mateusz Lewandowski <lewymati@users.noreply.github.com>
    GitOrigin-RevId: 0a7b6bb71fa17fce46c8b06812a1d24011095400

--- python/pathway/stdlib/utils/filtering.py
+++ python/pathway/stdlib/utils/filtering.py
@@ -8,12 +8,20 @@ import pathway.internals as pw
 def argmax_rows(
     table: pw.Table, *on: pw.ColumnReference, what: pw.ColumnExpression
 ) -> pw.Table:
-    filter = table.groupby(*on).reduce(argmax_id=pw.reducers.argmax(what))
-    return table.join(filter, table.id == filter.argmax_id, id=table.id).select(*table)
+    filter = (
+        table.groupby(*on)
+        .reduce(argmax_id=pw.reducers.argmax(what))
+        .with_id(pw.this.argmax_id)
+    )
+    return table.ix(filter.argmax_id).promise_universe_is_subset_of(table)
 
 
 def argmin_rows(
     table: pw.Table, *on: pw.ColumnReference, what: pw.ColumnReference
 ) -> pw.Table:
-    filter = table.groupby(*on).reduce(argmin_id=pw.reducers.argmin(what))
-    return table.join(filter, table.id == filter.argmin_id, id=table.id).select(*table)
+    filter = (
+        table.groupby(*on)
+        .reduce(argmin_id=pw.reducers.argmin(what))
+        .with_id(pw.this.argmin_id)
+    )
+    return table.ix(filter.argmin_id).promise_universe_is_subset_of(table)
