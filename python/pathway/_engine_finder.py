7452a9a6d8f92a1458ea4e3f9339a4a3a605c6fc
Michał Bartoszkiewicz <embe@pathway.com>
2024-03-26 08:00:12 +0100

    Make `_engine_finder.py` compatible with Rust 1.77 (#6049)
    GitOrigin-RevId: b8ba4460271b25f75690c60c09da20d28f3121ef

--- python/pathway/_engine_finder.py
+++ python/pathway/_engine_finder.py
@@ -1,34 +1,48 @@
 # Copyright © 2024 Pathway
 
+# mypy: disallow-untyped-defs, extra-checks, disallow-any-generics, warn-return-any
+
 from __future__ import annotations
 
 import json
 import subprocess
 import sys
+from collections.abc import Sequence
 from importlib.abc import MetaPathFinder
+from importlib.machinery import EXTENSION_SUFFIXES, ModuleSpec
 from importlib.util import spec_from_file_location
 from os import environ
 from pathlib import Path
+from types import ModuleType
 
 PROFILE_ENV_VAR = "PATHWAY_PROFILE"
 QUIET_ENV_VAR = "PATHWAY_QUIET"
 FEATURES_ENV_VAR = "PATHWAY_FEATURES"
 DEFAULT_PROFILE = "dev"
 
-RUST_PACKAGE = "pathway"
+RUST_CRATE = "pathway_engine"
 MODULE_NAME = "pathway.engine"
 
 
 class MagicCargoFinder(MetaPathFinder):
-    def find_spec(self, fullname, path, target=None):
+    def find_spec(
+        self,
+        fullname: str,
+        path: Sequence[str] | None,
+        target: ModuleType | None = None,
+    ) -> ModuleSpec | None:
         if fullname != MODULE_NAME:
             return None
 
-        module_path = cargo_build()
+        manifest_dir = Path(__file__).parent.parent.parent
+        module_path = cargo_build(manifest_dir)
         return spec_from_file_location(MODULE_NAME, module_path)
 
 
-def cargo_build():
+def cargo_build(manifest_dir: Path) -> Path:
+    assert not (manifest_dir / "__init__.py").exists()
+    manifest_file = manifest_dir / "Cargo.toml"
+    assert manifest_file.exists()
     profile = environ.get(PROFILE_ENV_VAR, DEFAULT_PROFILE)
     quiet = environ.get(QUIET_ENV_VAR, "0").lower() in ("1", "true", "yes")
     features = environ.get(FEATURES_ENV_VAR)
@@ -44,30 +58,28 @@ def cargo_build():
         args += ["--quiet"]
     if features:
         args += ["--features", features]
-    base_dir = Path(__file__).parent.parent
-    assert not (base_dir / "__init__.py").exists()
     cargo = subprocess.run(
         args,
         stdin=subprocess.DEVNULL,
         stdout=subprocess.PIPE,
-        cwd=base_dir,
+        cwd=manifest_dir,
         text=True,
         check=True,
     )
-    module_file = None
+    module_candidates = []
     for line in cargo.stdout.splitlines():
         data = json.loads(line)
         if data["reason"] != "compiler-artifact":
             continue
-        if not data["package_id"].startswith(RUST_PACKAGE + " "):
+        if data["target"]["name"] != RUST_CRATE:
             continue
         for filename in data["filenames"]:
             path = Path(filename)
-            if path.suffix != ".so":
+            if path.suffix not in EXTENSION_SUFFIXES:
                 continue
-            module_file = path
-    assert module_file is not None
-    return module_file
+            module_candidates.append(path)
+    assert len(module_candidates) == 1
+    return module_candidates[0]
 
 
 sys.meta_path.append(MagicCargoFinder())
