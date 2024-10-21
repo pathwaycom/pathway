aa380df402c899c36f8b58ef9b92913cff17de06
Sergey Kulik <104143901+zxqfd555-pw@users.noreply.github.com>
2024-10-21 15:54:44 +0200

    improve tech for debezium-postgresql example (#7499)
    GitOrigin-RevId: b8d0e0eb5038ff88ed3d2ec3c27555da3103cd2c

--- examples/projects/debezium-postgres-example/debezium/connector.sh
+++ examples/projects/debezium-postgres-example/debezium/connector.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-curl -H 'Content-Type: application/json' debezium:8083/connectors --data '
-{
-  "name": "values-connector",  
-  "config": {
-    "connector.class": "io.debezium.connector.postgresql.PostgresConnector", 
-    "plugin.name": "pgoutput",
-    "database.hostname": "postgres", 
-    "database.port": "5432", 
-    "database.user": "user", 
-    "database.password": "password", 
-    "database.dbname" : "values_db", 
-    "database.server.name": "postgres", 
-    "table.include.list": "public.values",
-    "database.history.kafka.bootstrap.servers": "kafka:9092",
-    "database.history.kafka.topic": "schema-changes.inventory"
-  }
-}'
+while true; do
+  http_code=$(curl -o /dev/null -w "%{http_code}" -H 'Content-Type: application/json' debezium:8083/connectors --data '{
+    "name": "values-connector",
+    "config": {
+      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+      "plugin.name": "pgoutput",
+      "database.hostname": "postgres",
+      "database.port": "5432",
+      "database.user": "user",
+      "database.password": "password",
+      "database.dbname" : "values_db",
+      "database.server.name": "postgres",
+      "table.include.list": "public.values",
+      "database.history.kafka.bootstrap.servers": "kafka:9092",
+      "database.history.kafka.topic": "schema-changes.inventory"
+    }
+  }')
+  if [ "$http_code" -eq 201 ]; then
+    echo "Debezium connector has been created successfully"
+    break
+  else
+    echo "Retrying Debezium connection creation in 1 second..."
+    sleep 1
+  fi
+done
